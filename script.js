8bf024a8b26994838fa62f2917828db3e0f603cc
LeBronsCuzConnie <constance.rosemond@csedge.org>
2019-03-02 11:57:31 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-$(document).ready(function(){
 // all jQuery methods go here...
 
 
+//Challenge 0
+$(".dance-icon").hover(onHovered, notHovered);
 
+function onHovered() {
+	//runningman-icon.pgn --> runningman.giphy.gif
+	//electroshuffle-let str = $(this)[0].src;
+	//taketheL-icon.png --> taketheL-giphy.gif
+	//$(this).substring(0,)
+	let str = $(this)[0].src;
+	let name = str.substring(0, str.length - 8);
+	$(this)[0].src = name + "giphy.gif";
+}
 
-
-
-
-
-
-
-
-
-
-
-});
+function notHovered() {
+	let str = $(this)[0].src;
+	let name = str.substring(0, str.length - 9);
+	$(this)[0].src = name + "icon.png";
+}
